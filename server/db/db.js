943ecdb7def6d066f0c28fa78063d91fdecd5796
Andrew Weber <andrew.robert.weber@gmail.com>
2015-08-17 17:37:30 -0700

    ready to go for heroku deploy

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@
 var mongoose = require( 'mongoose' ); 
 
 // Build the connection string 
-var dbURI = 'mongodb://localhost/electevent'; 
+var dbURI = 'mongodb://heroku_0wg7zv0m:rsjr5clbacal6lt7b4fnpm67ug@ds035663.mongolab.com:35663/heroku_0wg7zv0m'; 
 
 // Create the database connection 
 var db = mongoose.connect(dbURI); 
